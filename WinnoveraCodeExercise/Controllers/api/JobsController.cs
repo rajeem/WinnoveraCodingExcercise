b8b85e63d0e1445af51dbf7274788d72ca840421
rajeemcariazo-t450 <rajeem_cariazo@yahoo.com>
2018-10-30 22:14:32 +0800

    Add project files.

---
+++ WinnoveraCodeExercise/Controllers/api/JobsController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WinnoveraCodeExercise.Controllers.api
+{
+    public class Job
+    {
+        public string id { get; set; }
+        public string created_at { get; set; }
+        public string title { get; set; }
+        public string location { get; set; }
+        public string type { get; set; }
+        public string description { get; set; }
+        public string company { get; set; }
+    }
+
+    [Route("api/[controller]")]
+    [ApiController]
+    public class JobsController : ControllerBase
+    {
+        [HttpGet]
+        public async Task<List<Job>> GetAll()
+        {
+            var client = new HttpClient();
+            client.BaseAddress = new Uri("https://jobs.github.com");
+
+            var response = await client.GetAsync("positions.json");
+            return await response.Content.ReadAsAsync<List<Job>>();
+        }
+    }
+}
---
+++ WinnoveraCodeExercise/wwwroot/js/site.js
@@ -0,0 +1,24 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+
+function toggleShow(sender) {
+    $(sender).closest('div').find('.job-body').toggle();
+    $(sender).toggleClass('glyphicon-chevron-right');
+    $(sender).toggleClass('glyphicon-chevron-down');
+}
+
+$(function () {
+    $.ajax({
+        type: 'GET',
+        url: '/api/jobs',
+        success: function (res) {
+            if (res) {
+                for (var i = 0; i < res.length; i++) {
+                    $('#content').append('<div><p class="job-title glyphicon glyphicon-chevron-right" onclick="toggleShow(this)">' + res[i].title + '</p><div class="job-body" style="display:none">' + res[i].description + '</div></div>');
+                }
+            }
+        }
+    })
+})
